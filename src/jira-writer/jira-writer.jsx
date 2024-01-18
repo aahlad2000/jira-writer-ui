d41821bdfcb7346742f9014582c26f1bf5242fc4
Aahlad <aahladkethineeedi@gmail.com>
2024-01-18 22:26:20 +0530

    Added copy to clipboard button and fixed response not rendering in textarea

--- src/jira-writer/jira-writer.jsx
+++ src/jira-writer/jira-writer.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState } from "react";
 import "./jira-writer.css";
+import Loading from "../loading/Loading";
 
 const JiraWriter = () => {
   const queryRef = useRef(null);
   const [hideResultBox, setHideResultBox] = useState(true);
   const [hideValidation, setValidation] = useState(true);
   const [fetchedValue, setFetchedValue] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   function checkValidation(enteredQuery) {
     if (enteredQuery.toLowerCase().includes("write a jira about")) {
@@ -17,31 +19,32 @@ const JiraWriter = () => {
     }
   }
 
-  async function handleClick() {
+  function handleClick() {
     const enteredQuery = queryRef.current.value;
     const shouldMakeAPICall = checkValidation(enteredQuery);
     if (shouldMakeAPICall) {
+      setLoading(true);
       try {
-        const response = await fetch(
-          "http://localhost:8080/get/openAiResponse",
-          {
-            mode: "no-cors",
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(enteredQuery),
-          }
-        );
-
-        responseHandler(hideValidation);
-
-        if (response.ok) {
-          setFetchedValue(response);
-        } else {
-          setFetchedValue("Error occured while calling OpenAI API");
-        }
+        fetch("http://localhost:8080/get/openAiResponse", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(enteredQuery),
+        })
+          .then((response) => {
+            return response.text();
+          })
+          .then((data) => {
+            setLoading(false);
+            console.log("fetched the resposne: ", data);
+            responseHandler(hideValidation);
+            setFetchedValue(data);
+          });
       } catch (exception) {
+        setLoading(false);
+        responseHandler(hideValidation);
+        console.log(exception);
         alert("Sorry, unable to get response OpenAI from API");
       }
     } else {
@@ -50,7 +53,7 @@ const JiraWriter = () => {
   }
 
   function responseHandler(hideValidation) {
-    if (!hideValidation) {
+    if (hideValidation) {
       setHideResultBox(false);
     } else {
       setHideResultBox(true);
@@ -61,19 +64,30 @@ const JiraWriter = () => {
     <div className="jira-writer-component">
       <h1 className="heading">Jira Writer</h1>
       <br></br>
-      <div className="query-box">
-        <p>Describe what your Jira is about:</p>
-        <textarea ref={queryRef}></textarea>
+      <div className="container" hidden={isLoading}>
+        <div className="query-box">
+          <p>Describe what your Jira is about:</p>
+          <textarea ref={queryRef}></textarea>
+        </div>
+        <br></br>
+        <button onClick={handleClick}>SUBMIT</button>
+        <br></br>
+        <br></br>
+        <small hidden={hideValidation} className="validtion-tag">
+          Please use the correct query format
+        </small>
+        <div className="response-box" hidden={hideResultBox}>
+          <button
+            id="copy-to-clipboard"
+            onClick={() => navigator.clipboard.writeText(fetchedValue)}
+          >
+            Copy
+          </button>
+          <textarea value={fetchedValue} readOnly></textarea>
+        </div>
       </div>
-      <br></br>
-      <button onClick={handleClick}>SUBMIT</button>
-      <br></br>
-      <br></br>
-      <small hidden={hideValidation} className="validtion-tag">
-        Please use the correct query format
-      </small>
-      <div className="response-box" hidden={hideResultBox}>
-        <textarea value={fetchedValue} readOnly></textarea>
+      <div className="loading-spinner" hidden={!isLoading}>
+        <Loading />
       </div>
     </div>
   );
